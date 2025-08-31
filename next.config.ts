eac6535e23849cc49ca1e9bf7bde99f0ec83d410
mumar022345678-beep <mumar022345678@gmail.com>
2025-08-31 12:15:17 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["voob5s3q1p.ufs.sh", "utfs.io"],
+  },
+  reactStrictMode: false
+};
+
+export default nextConfig;
